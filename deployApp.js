56bc1d51af93b6e967069f80519863c3f306f296
Mansoor Hussain <hussainmansoor876@gmail.com>
2023-05-17 18:45:45 +0500

    changes done

--- deployApp.js
+++ deployApp.js
@@ -35,50 +35,57 @@ const deployApp = () => {
         service_account_json: service_key
       }
 
-      console.log('db creating')
+      console.log('db config')
 
-      exec(`gcloud services enable sqladmin.googleapis.com && gcloud compute addresses create google-managed-services-default --global --prefix-length=16 --description="peering range for Google" --network=default --purpose=VPC_PEERING`, (error, stdout, stderr) => {
+      exec(`gcloud services enable sqladmin.googleapis.com && gcloud services enable cloudresourcemanager.googleapis.com && gcloud compute addresses create google-managed-services-default --global --prefix-length=16 --description="peering range for Google" --network=default --purpose=VPC_PEERING`, (error, stdout, stderr) => {
+
+        console.log('vpc', stdout, error)
         // 
-        exec(`gcloud sql instances create ${dbName} --database-version=POSTGRES_14 --cpu=1 --memory=3840MiB --storage-size=20480MiB --network=default --no-assign-ip --region=${region}`, (error, stdout, stderr) => {
+        exec(`gcloud services vpc-peerings connect --service=servicenetworking.googleapis.com --ranges=google-managed-services-default --network=default`, (error, stdout, stderr) => {
 
-          console.log('stdout', stdout, error)
+          console.log('stdout network db creating', stdout, error)
 
-          try {
-            axios.post(apiUrl, body)
-              .then((res) => {
-                const { data } = res
-                console.log('api response', data)
-              })
-              .catch((e) => console.log('api error', e))
-          }
-          catch (e) {
-            console.log('api catch', e)
-          }
+          exec(`gcloud sql instances create ${dbName} --database-version=POSTGRES_14 --cpu=1 --memory=3840MiB --storage-size=20480MiB --network=default --no-assign-ip --region=${region}`, (error, stdout, stderr) => {
 
-          let privateIp = stdout?.split(' ')?.filter(v => v).slice(-2,)[0]
-          console.log('privateIp', privateIp)
+            console.log('stdout', stdout, error)
 
-          exec(`gcloud sql users set-password postgres --instance=${dbName} --password=postgres`, (error, stdout, stderr) => {
-            console.log('password updated', stdout, error)
-          })
+            try {
+              axios.post(apiUrl, body)
+                .then((res) => {
+                  const { data } = res
+                  console.log('api response', data)
+                })
+                .catch((e) => console.log('api error', e))
+            }
+            catch (e) {
+              console.log('api catch', e)
+            }
 
-          exec(`gsutil mb gs://${bucketName} && gsutil uniformbucketlevelaccess set off gs://${bucketName}`, (error, stdout, stderr) => {
-            console.log('storage', stdout)
-          })
+            let privateIp = stdout?.split(' ')?.filter(v => v).slice(-2,)[0]
+            console.log('privateIp', privateIp)
 
-          exec(`gcloud compute networks vpc-access connectors create doc-ai-vpc --region=${region} --network=default --range=10.0.0.0/28 --min-instances=2 --max-instances=10 --machine-type=e2-micro`, (error, stdout, stderr) => {
-            console.log('stdout: db', stdout, error)
-          })
+            exec(`gcloud sql users set-password postgres --instance=${dbName} --password=postgres`, (error, stdout, stderr) => {
+              console.log('password updated', stdout, error)
+            })
 
-          exec(`gcloud builds submit --tag ${imageUrl} --timeout=9000 --machine-type=n1-highcpu-32`, (error, stdout, stderr) => {
-            exec(`gcloud run deploy ${appName} --image=${imageUrl}:latest --set-env-vars "^@^ALLOWED_ORIGIN=https://doc-ai-znp7f527ca-uc.a.run.app@DB_USER=postgres@DB_PASSWORD=postgres@DB_HOST=${privateIp}@storage_bucket=${bucketName}" --set-cloudsql-instances=${cloudInstance} --vpc-connector=projects/${projectId}/locations/${region}/connectors/doc-ai-vpc --allow-unauthenticated --region=${region} --project=${projectId} && gcloud run services update-traffic ${appName} --to-latest --region=${region}`, (error, stdout, stderr) => {
+            exec(`gsutil mb gs://${bucketName} && gsutil uniformbucketlevelaccess set off gs://${bucketName}`, (error, stdout, stderr) => {
+              console.log('storage', stdout)
+            })
+
+            exec(`gcloud compute networks vpc-access connectors create doc-ai-vpc --region=${region} --network=default --range=10.0.0.0/28 --min-instances=2 --max-instances=10 --machine-type=e2-micro`, (error, stdout, stderr) => {
+              console.log('stdout: db', stdout, error)
+            })
+
+            exec(`gcloud builds submit --tag ${imageUrl} --timeout=9000 --machine-type=n1-highcpu-32`, (error, stdout, stderr) => {
+              exec(`gcloud run deploy ${appName} --image=${imageUrl}:latest --set-env-vars "^@^ALLOWED_ORIGIN=https://doc-ai-znp7f527ca-uc.a.run.app@DB_USER=postgres@DB_PASSWORD=postgres@DB_HOST=${privateIp}@storage_bucket=${bucketName}" --set-cloudsql-instances=${cloudInstance} --vpc-connector=projects/${projectId}/locations/${region}/connectors/doc-ai-vpc --allow-unauthenticated --region=${region} --project=${projectId} && gcloud run services update-traffic ${appName} --to-latest --region=${region}`, (error, stdout, stderr) => {
 
 
-              console.log('stdout***', stdout?.split(' ')?.filter(v => v)?.[1]?.split('\r\n')[0])
-              const HOST = stdout?.split(' ')?.filter(v => v)?.[1]?.split('\r\n')[0]
+                console.log('stdout***', stdout?.split(' ')?.filter(v => v)?.[1]?.split('\r\n')[0])
+                const HOST = stdout?.split(' ')?.filter(v => v)?.[1]?.split('\r\n')[0]
 
-              exec(`gcloud run deploy ${appName} --image=${imageUrl}:latest --set-env-vars "^@^ALLOWED_ORIGIN=${HOST}@DB_USER=postgres@DB_PASSWORD=postgres@DB_HOST=${privateIp}@storage_bucket=${bucketName}" --set-cloudsql-instances=${cloudInstance} --vpc-connector=projects/${projectId}/locations/${region}/connectors/doc-ai-vpc --allow-unauthenticated --region=${region} --project=${projectId} && gcloud run services update-traffic ${appName} --to-latest --region=${region}`, (error, stdout, stderr) => {
-                console.log('stdout: cloud run', stdout)
+                exec(`gcloud run deploy ${appName} --image=${imageUrl}:latest --set-env-vars "^@^ALLOWED_ORIGIN=${HOST}@DB_USER=postgres@DB_PASSWORD=postgres@DB_HOST=${privateIp}@storage_bucket=${bucketName}" --set-cloudsql-instances=${cloudInstance} --vpc-connector=projects/${projectId}/locations/${region}/connectors/doc-ai-vpc --allow-unauthenticated --region=${region} --project=${projectId} && gcloud run services update-traffic ${appName} --to-latest --region=${region}`, (error, stdout, stderr) => {
+                  console.log('stdout: cloud run', stdout)
+                })
               })
             })
           })
