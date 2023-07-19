6a8580e0d8663f2cd17663d5508f2c29cf7351cc
Mansoor Hussain <hussainmansoor876@gmail.com>
2023-07-19 05:21:12 +0500

    updated

--- helpers/index.js
+++ helpers/index.js
@@ -112,10 +112,26 @@ const getUniqueArrayOfObjects = (ary, objectPropertName) => {
     })
 }
 
+async function getServiceAccountDetails() {
+    const auth = new googleAuth.GoogleAuth({
+        scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+    });
+    const client = await auth.getClient();
+
+    const projectId = await auth.getProjectId();
+    const url = `https://iam.googleapis.com/v1/projects/${projectId}/serviceAccounts/${client.email}`;
+
+    const res = await client.request({ url });
+
+    console.log('Service account details:', res.data);
+}
+
+
 const getAuthUrl = async (uri, storage) => {
     if (uri && uri.length) {
         try {
             console.log('uri', uri)
+            getServiceAccountDetails()
             const expires = moment(moment(), 'MM-DD-YYYY').add(2, 'days')
             const bucketName = uri.split('/')[2]
             const myBucket = storage.bucket(bucketName)
